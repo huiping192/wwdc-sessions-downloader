d93bb980da571cebbeb010b233823de3552a5449
huiping_guo <huiping.guo@dena.jp>
2022-08-08 22:20:36 +0900

    init2

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import requests
 
-print("hello world")
+wwdc2022_url = "https://developer.apple.com/wwdc22/sessions/"
+
+ret = requests.get(wwdc2022_url)
+
+print(ret.url)
+print(ret.text)
+
+
+def get_all_video_urls():
+    return ["a"]
+
+def get_video_download_urls(session_detail_url):
+    return ["a"]
+
+def download_file(url):
+    print("a")
